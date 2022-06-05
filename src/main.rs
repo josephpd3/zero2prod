5b92d6f4b732be83cf9f70226c27910e3587812f
Joseph DeBartola <josephdebartola@gmail.com>
2022-06-05 05:15:05 -0500

    Add tests for subscriptions route, add script for bootstrapping postgres, add sqlx and first migration for subscription table, refactor source into separate modules

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 //! main.rs
 
-use zero2prod::run;
+use std::net::TcpListener;
+
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000")
+        .expect("Failed to bind to port 8000.");
+    println!("Listening at 127.0.0.1:8000...");
+    run(listener)?.await
 }
